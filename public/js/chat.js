23beefe5c2a9aa9023bccb70d9bd4c5553bbe73d
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-09-15 16:33:09 +0200

    Acknowledgements and Location

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,5 +9,27 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message);
+    socket.emit('sendMessage', message, (error) => {
+        if(error) {
+            return console.log(error);
+        }
+
+        console.log('message deliverd!');
+    });
 });
+
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if(!navigator.geolocation) {
+        return alert('GEOLOCATION IS NOT SUPPORTED BY YOUR BROWSER!!');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!');
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 
 const app = express();
@@ -18,9 +19,25 @@ io.on('connection', (socket) => {
     console.log('new web sokect connection !!');
 
     socket.emit('message', 'wellcome');
+    socket.broadcast.emit('message', 'new user has joined!');
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, call) => {
+        const filter = new Filter();
+
+        if(filter.isProfane(message)) {
+            return call('Profanity is not allowed');
+        }
         io.emit('message', message);
+        call();
+    });
+
+    socket.on('sendLocation', (coords, call) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+        call();
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'user has left!');
     });
 });
 
