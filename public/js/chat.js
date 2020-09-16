aee017df219a2f9c31777a6be08f81467f9430b9
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-09-16 03:00:39 +0200

    form and buttons status

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,27 @@
 const socket = io();
 
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+
+const $sendLocation = document.querySelector('#send-location');
+
 socket.on('message', (wellcome) => {
     console.log(wellcome);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    //disable sending message till its sent
+    $messageFormButton.setAttribute('disabled', 'disabled');
 
     const message = e.target.elements.message.value
 
     socket.emit('sendMessage', message, (error) => {
+        
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
         if(error) {
             return console.log(error);
         }
@@ -24,12 +36,16 @@ document.querySelector('#send-location').addEventListener('click', () => {
         return alert('GEOLOCATION IS NOT SUPPORTED BY YOUR BROWSER!!');
     }
 
+    $sendLocation.setAttribute('disabled', 'disabled');
+
     navigator.geolocation.getCurrentPosition((position) => {
         socket.emit('sendLocation', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         }, () => {
             console.log('Location shared!');
+
+            $sendLocation.removeAttribute('disabled');
         })
     })
 })
